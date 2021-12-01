7dc37d1f14fc16b00b286f1ad4f52337674e78a5
pooja sonji <poojasonji42314@gmail.com>
2021-12-01 17:21:41 +0530

    I do the changes.

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Exploring GIT");
+            Console.WriteLine("Hi I am pooja do the changes ");
         }
     }
 }
